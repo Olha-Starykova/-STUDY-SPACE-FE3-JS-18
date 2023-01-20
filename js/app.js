5598263ba8866bd008051f75ce62ea5c9b229866
Olha Starykova <olenkalog@gmail.com>
2023-01-20 06:38:05 +0000

    lesson_20

--- js/app.js
+++ js/app.js
@@ -2,90 +2,86 @@
 
 // Lesson 20
 //Task 1
-// let firstUserNumber = prompt("Insert the first number", 0);
-// let secondUserNumber = prompt("Insert the second number", 0);
+let firstUserNumber = prompt("Insert the first number", 0);
+let secondUserNumber = prompt("Insert the second number", 0);
 
-// let firstNumber = Number(firstUserNumber);
-// let secondNumber = Number(secondUserNumber);
+let firstNumber = Number(firstUserNumber);
+let secondNumber = Number(secondUserNumber);
 
-// function maxNumber (a, b) {
-//     if (a > b) {
-//         return alert(`Greater number ${a}`);
-//     }
-//     if (a < b) {
-//         return alert(`Greater number ${b}`);
-//     }
-//     if (a = b) {
-//         return alert(`The numbers are equal ${a} = ${b}`);
-//     }
-//     return alert(`You lose`);
-// };
+function maxNumber (a, b) {
+    if (a > b) {
+        return alert(`Greater number ${a}`);
+    }
+    if (a < b) {
+        return alert(`Greater number ${b}`);
+    }
+    if (a = b) {
+        return alert(`The numbers are equal ${a} = ${b}`);
+    }
+    return alert(`You lose`);
+};
 
-// maxNumber(firstNumber, secondNumber);
+maxNumber(firstNumber, secondNumber);
 
 
 //Task 2
 
-//  let enterUserNumber = prompt("Enter the numder", 1);
-//  let enterNumber = Number(enterUserNumber);
-//  let reversNumber = 0;
+ let enterUserNumber = prompt("Enter the numder", 1);
+ let enterNumber = Number(enterUserNumber);
+ let reversNumber = 0;
 
-// function revers (enterNumber) {
-//     reversNumber = enterNumber*(-1);
-//     return alert(reversNumber);
-// }
+function revers (enterNumber) {
+    reversNumber = enterNumber*(-1);
+    return alert(reversNumber);
+}
 
-// revers(enterNumber);
+revers(enterNumber);
 
 //Task 3
-// let numberUser = prompt("Insert the number", 1);
-// let number = Number(numberUser);
+let numberUser = prompt("Insert the number", 1);
+let number = Number(numberUser);
 
-// let countUser = prompt("Insert the count", 1);
-// let count = Number(countUser);
+let countUser = prompt("Insert the count", 1);
+let count = Number(countUser);
 
-// let element = 0;
-// function calcTotalCount (number, count) {
-//     for (let i = 1; i <= count; i++) {
-//          element = (number + 3)*i;
-//     }
-//     return (`Total count ${element}`) ;
-// };
+let element = 0;
+function calcTotalCount (number, count) {
+    for (let i = 1; i <= count; i++) {
+         element = (number + 3)*i;
+    }
+    return (`Total count ${element}`);
+};
 
-// alert (calcTotalCount(number, count));
+alert (calcTotalCount(number, count));
 
 //Task 4
 
-let kmUser = prompt("Insert the distance", 1);
+let kmUser = prompt("Insert the distance, km", 1);
 let km = Number(kmUser);
-console.log(km)
 
-let metricUser = prompt('Insert the metric', " ");
+let metricUser = prompt('Insert the metric', "m, cm");
 let metric = String (metricUser);
-console.log(metric)
 
-function getMetric(metric, km, m = 1, cm = 1) {
+alert(getMetric(metric, km));
+
+function getMetric(metric, km) {
     if (metric === 'm') {
-        console.log(`это km  ${km}`)
-        console.log(`это metric  ${metric}`)
-           kmToMeter(km)
-    };
+          return kmToMeter(km)
+    } else
     if (metric === 'cm') {
-          kmToCm(km)
-    }
-  
-} 
+         return kmToCm(km)
+    };
+         return `Your game is over`
+}; 
 
 function kmToMeter(km) {
     let resalt = km*1000
-    console.log(`это km col ${km}`)
-    console.log(`это metric col ${metric}`)
-    return  alert(`${km} is ${resalt}`)
-}
+    return  (`${km}km is ${resalt}m`)
+};
 
 function kmToCm(km) {
     let resalt = km*100000
-    return alert(`${km} is ${resalt}`)
+    return (`${km}km is ${resalt}cm`)
 
-}
-alert(getMetric(metric, km))
+};
+
