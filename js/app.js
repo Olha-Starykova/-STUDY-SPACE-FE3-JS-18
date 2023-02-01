8f8e30d778ba9ab465ae6c7800b5a8c3f8fb3d00
Olha Starykova <olenkalog@gmail.com>
2023-02-01 20:53:16 +0000

    added lesson_24

--- js/app.js
+++ js/app.js
@@ -1,26 +1,110 @@
-// Lesson 18
+'use strict'
+// Lesson 24
 
 //Task 1
-let firstMonth = prompt("Enter fierst month salary", '1');
-let secondtMonth = prompt("Enter second month salary", '2');
-let sum = +firstMonth + +secondtMonth
-alert(`Your salary for 2 month is:  ${sum}$`);
+
+// class User{
+//   #name;
+//   constructor(name, login, age){
+//     this.#name = name;
+//     this.login = login;
+//     this.age = age;
+//   }
+// }
+
+// let user1 = new User('Mike', 'MK_18', 18);
+// let user2 = new User('', 'NRG', 22);
+
+// console.log(user1.login);//MK_18;
+// console.log(user1.age);//18;
+// console.log(user2.login);//NRG;
+// console.log(user2.age);//22;
+// console.log(user2);//22;
 
 //Task 2
-alert(`Your have extra bonus! Your total salary was: ${sum}$  It's increase for 1$ and now it is: ${++sum}$`);
+
+// class User{
+//   #name;
+//   constructor(name, login, age){
+//     this.#name = name;
+//     this.login = login;
+//     this.age = age;
+//   }
+// getName(isAdmin){
+// switch(isAdmin){
+//   case true:
+//    return this.#name ? this.#name : this.login;
+//   case false:
+//     return `Permission denied`;    
+//     };
+//   };
+// };
+
+// let user1 = new User('Mike', 'MK_18', 18);
+// let user2 = new User('', 'NRG', 22);
+
+// console.log(user1.getName(true)) //Mike
+// console.log(user2.getName(true)) //NRG
+// console.log(user2.getName(false)) //Permission denied
+
 
 //Task 3
-let sumBonus = +firstMonth + +secondtMonth + 1;
-// alert(`Will you work next? ${sumBonus >= 2000}  `);
 
-//Task 4
-((sumBonus >= 2000) && !alert(`I'm ready to work`)) || !alert(`I'm out`);
+class User{
+  #name;
+  constructor(name, login, age){
+    this.#name = name;
+    this.login = login;
+    this.age = age;
+  }
+    getName(isAdmin){
+    switch(isAdmin){
+      case true:
+      return this.#name ? this.#name : this.login;
+      case false:
+        return `Permission denied`;    
+        };
+      };
+
+    changeName(newname){
+      let password = prompt("Enter your password", 123);
+
+      if(+password === 123){
+        console.log( `Name Changed from ${this.#name} to ${newname}`);
+        this.#name = newname;
+      } else {
+        console.log(`Permission denied`);   
+      }
+    }
+};
+
+  let user1 = new User('Mike', 'MK_18', 18);  
+  user1.changeName('Bill', '123'); //Name Changed from Mike to Bill
 
 
-// if (sumBonus >= 2000) {
-//       alert(`I'm ready to work`) 
-// } else {
-//    alert(`I'm out`) 
-// } 
+//Task 5
 
-console.log(sumBonus >= 2000);
+class UserNew{
+  #phone;
+  constructor(name, phone){
+    this.#phone = phone;
+    this.name = name;
+  };
+  getPhone(isAdmin){
+    let arr = this.#phone.split('-');
+    arr.splice(1, 2, "******");
+    let str = arr.join('-'); 
+   
+    return isAdmin ? this.#phone : str;
+  }
+};
+
+let user3 = new UserNew('Mike', '067-888-88-99');
+let user4 = new UserNew('Tom', '099-888-88-99');
+
+console.log(user3.getPhone(false))//067-******-99
+console.log(user4.getPhone(false))//099-******-99
+console.log(user3.getPhone(true))//067-888-88-99
+console.log(user4.getPhone(true))//067-888-88-99
+
+
