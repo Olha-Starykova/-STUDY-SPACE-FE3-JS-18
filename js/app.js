5c9d3dc386de40790066aab00ec8f609286b591e
Olha Starykova <olenkalog@gmail.com>
2023-02-05 21:54:14 +0000

    added lesson_25

--- js/app.js
+++ js/app.js
@@ -1,26 +1,121 @@
-// Lesson 18
+'use strict'
+// Lesson 25
 
 //Task 1
-let firstMonth = prompt("Enter fierst month salary", '1');
-let secondtMonth = prompt("Enter second month salary", '2');
-let sum = +firstMonth + +secondtMonth
-alert(`Your salary for 2 month is:  ${sum}$`);
 
-//Task 2
-alert(`Your have extra bonus! Your total salary was: ${sum}$  It's increase for 1$ and now it is: ${++sum}$`);
+let user = {};
+
+Object.defineProperties(user, {
+  'name': {
+    value: 'Max',
+    enumerable: true,
+    configurable: true,
+  },
+  'age': {
+    value: 30,
+    enumerable: true,
+    configurable: true,
+  },
+  'id': {
+    value: Math.random(),
+    enumerable: true,
+  }
+})
+
+console.log(Object.getOwnPropertyDescriptor(user, 'name'))//{value: 'Max', writable: false, enumerable: true, configurable: true}
+console.log(Object.getOwnPropertyDescriptor(user, 'age'))//{value: 30, writable: false, enumerable: true, configurable: true}
+console.log(Object.getOwnPropertyDescriptor(user, 'id'))//{value: 0.8676334570075539, writable: false, enumerable: true, configurable: false}
+
+// //Task 2
+
+let dataBase = {
+  dbName: 'user',
+  dbTape: 'MySQL'
+};
+
+let configurateDB ={
+  token: '123',
+  password: '567',
+  user: 'admin'
+};
+
+Object.freeze(dataBase);
+console.log(Object.isFrozen(dataBase)) // true
+Object.seal(configurateDB);
+console.log(Object.isSealed(configurateDB)) // true
+
 
 //Task 3
-let sumBonus = +firstMonth + +secondtMonth + 1;
-// alert(`Will you work next? ${sumBonus >= 2000}  `);
+
+let salaries = {
+  frontend: 2000,
+  backend: 1500,
+  design: 1000,
+};
+
+Object.defineProperty(salaries,'addSalaries', {
+  set (value){
+   
+    value.map(e => {
+    let userInfo = e.split(': ')
+    
+   switch(userInfo[0]){
+      case 'frontend':
+      this.frontend = +userInfo[1] 
+      case 'backend':
+      this.backend = +userInfo[1]  
+      case 'design':
+      this.design = +userInfo[1]  
+      case 'manager':
+      this.manager = +userInfo[1]  
+      default:
+      this[userInfo[0]] = +userInfo[1] ;
+  }
+    })
+  }
+} )
+
+
+Object.defineProperty(salaries, 'sum', {
+  get (){
+      let sum = 0;
+      for(let salary in this){
+          sum += this[salary];
+      }
+      console.log(sum);
+  },
+  enumerable: false,
+
+})
+
+ salaries.sum //4500
+ salaries.addSalaries = ['frontend: 2500', 'backend: 1750', 'design: 1300', 'manager: 800'];
+ salaries.sum //6350
+ salaries.addSalaries = ['frontend: 2500', 'backend: 1750', 'design: 1300', 'manager: 800', 'boss: 3000'];
+ salaries.sum //9350
+ console.log(salaries)// {frontend: 2500, backend: 1750, design: 1300, manager: 800, boss: 3000}
 
 //Task 4
-((sumBonus >= 2000) && !alert(`I'm ready to work`)) || !alert(`I'm out`);
 
+let userNew = {
+  name: 'Mike',
+  surname: 'Davis',
+  age: 25,
+}
+ 
+Object.defineProperty(userNew, 'userInfo', {
+  get (){
+    const arr = [];
+    for (let key in userNew){
+      arr.push(`${key}: ${userNew[key]}`)
+    }
+    console.log(arr.join(', '))
 
-// if (sumBonus >= 2000) {
-//       alert(`I'm ready to work`) 
-// } else {
-//    alert(`I'm out`) 
-// } 
+  }, 
+  enumerable: false,
+})
 
-console.log(sumBonus >= 2000);
+userNew.userInfo; // name: Mike, surname: Davis, age: 25
+userNew.login = "MK_18"
+userNew.userInfo; // name: Mike, surname: Davis, age: 25, login: MK_18
+
