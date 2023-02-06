c9751c3b8ff7f0daddfbd9c2e8aa67ceedb32af7
Olha Starykova <olenkalog@gmail.com>
2023-02-06 02:49:39 +0000

    addition lesson_26

--- js/app.js
+++ js/app.js
@@ -1,26 +1,89 @@
-// Lesson 18
+'use strict'
+// Lesson 26
 
 //Task 1
-let firstMonth = prompt("Enter fierst month salary", '1');
-let secondtMonth = prompt("Enter second month salary", '2');
-let sum = +firstMonth + +secondtMonth
-alert(`Your salary for 2 month is:  ${sum}$`);
+
+
+let car = {
+  type: 'electric',
+  wheels: 4,
+}
+
+let sportCar = {
+  doors: 2,
+}
+
+Object.setPrototypeOf(sportCar, car);
+// console.log(sportCar.type) // electric
+console.log(Object.getPrototypeOf(sportCar) === car);//true
+
+let passengerCar = Object.create(car)  
+passengerCar.doors = 4;
+
+console.log(passengerCar.type)// electric
+
+let toyCar = Object.create(sportCar)
+toyCar.type = 'toy'
+console.log(Object.getPrototypeOf(toyCar) === sportCar);//true
+console.log(Object.getPrototypeOf(sportCar) === car);//true
+console.log(toyCar.type)//toy
+console.log(toyCar.wheels)//4
+console.log(toyCar.doors)//2
 
 //Task 2
-alert(`Your have extra bonus! Your total salary was: ${sum}$  It's increase for 1$ and now it is: ${++sum}$`);
+
+let employees = {
+  pay(munth, sum){
+    this.wallet[munth] = sum;
+    }
+};
+
+let frontendDeveloper = {
+  name: 'Mike',
+  wallet: {},
+}
+Object.setPrototypeOf(frontendDeveloper, employees)
+
+let beckendDeveloper = {
+  name: 'Bob',
+  wallet: {},
+};
+
+Object.setPrototypeOf(beckendDeveloper, employees)
+beckendDeveloper.pay('june', 1500);
+
+console.log(beckendDeveloper.wallet.june); //1500
+console.log(frontendDeveloper.wallet.june); //undefined
 
 //Task 3
-let sumBonus = +firstMonth + +secondtMonth + 1;
-// alert(`Will you work next? ${sumBonus >= 2000}  `);
+
+function User(name, age){
+  this.name = name;
+  this.age = age;
+};
+
+let user_1 = new User('Mike', 18)
+let user_2 = new user_1.constructor('Bob', 25)
+
+console.log(user_1)//User {name: 'Mike', age: 18}
+console.log(user_2)//User {name: 'Bob', age: 25}
 
 //Task 4
-((sumBonus >= 2000) && !alert(`I'm ready to work`)) || !alert(`I'm out`);
 
+function UserType(name){
+  for(let i =0; i < name.length; ++i){
+    this[i] = name[i];
+    this.join = Array.prototype.join;
+    if(i + 1 == name.length){
+      Object.defineProperty(this, 'length', {
+        value: i + 1,
+        writable: false,
+        enumerable: true,
+        configurable: true,
+      });
+    }
+  }
+};
 
-// if (sumBonus >= 2000) {
-//       alert(`I'm ready to work`) 
-// } else {
-//    alert(`I'm out`) 
-// } 
-
-console.log(sumBonus >= 2000);
+let admins = new UserType(['Mike', 'Bob', 'Nikola']);
+console.log(admins.join('; ')); //Mike; Bob; Nikola
