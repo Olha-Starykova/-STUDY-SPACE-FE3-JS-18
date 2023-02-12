075d3622278046db6a03aa5e79eeb49aac082232
Olha Starykova <olenkalog@gmail.com>
2023-02-12 05:14:25 +0000

    added lesson_29

--- js/app.js
+++ js/app.js
@@ -1,26 +1,78 @@
-// Lesson 18
+'use strict'
+// Lesson 29
 
 //Task 1
-let firstMonth = prompt("Enter fierst month salary", '1');
-let secondtMonth = prompt("Enter second month salary", '2');
-let sum = +firstMonth + +secondtMonth
-alert(`Your salary for 2 month is:  ${sum}$`);
+
+// let h2 = document.querySelectorAll('h2')
+// console.log(h2[1].innerHTML)
+
+// h2[1].textContent = 'DOM method description'
+// console.log(h2[1].innerHTML)
 
 //Task 2
-alert(`Your have extra bonus! Your total salary was: ${sum}$  It's increase for 1$ and now it is: ${++sum}$`);
+
+let ua = document.querySelectorAll('a')
+
+function isUa(arr){
+for (let i = 0; i < arr.length; i++){
+let arrSplit   = arr[i].href.split('/')
+for (let index = 0; index < arrSplit.length; index++) {
+  const element = arrSplit [index];
+if(arrSplit[index] === 'ua'){
+let linkUa = arrSplit .join('/')
+console.log(linkUa)
+    }
+  }
+}
+}
+ isUa(ua)
+//  http://127.0.0.1:5500/site.ua/ua/page_1
+//  http://127.0.0.1:5500/site.ua/ua/page_2
+//  http://127.0.0.1:5500/site.ua/ua/page_3
 
 //Task 3
-let sumBonus = +firstMonth + +secondtMonth + 1;
-// alert(`Will you work next? ${sumBonus >= 2000}  `);
+
+let list = document.querySelector('#ul');
+console.log(list)
+
+let firstLi = document.querySelector('#first');
+console.log(firstLi)
+
+let thirdLi = document.querySelector('#third')
+console.log(thirdLi)
+
+let liNull = document.createElement('li');
+console.log(liNull)
+liNull.setAttribute('id', 'null');
+console.log(liNull.outerHTML);
+liNull.innerHTML = 0;
+list.prepend(liNull);
+
+let liSecond = document.createElement('li');
+console.log(liSecond)
+liSecond.setAttribute('id', 'second');
+console.log(liSecond.outerHTML);
+liSecond.innerHTML = 2;
+thirdLi.replaceWith(liSecond);
+
+let nowLiThird = document.createElement('li');
+nowLiThird.setAttribute('id', 'third');
+nowLiThird.innerHTML = 3;
+list.append(nowLiThird);
+
+let liFourth = document.createElement('li');
+liFourth.setAttribute('id', 'fourth');
+liFourth.innerHTML = 4;
+list.append(liFourth);
 
 //Task 4
-((sumBonus >= 2000) && !alert(`I'm ready to work`)) || !alert(`I'm out`);
 
+let h1 = document.querySelector('h1')
+console.log(h1.outerHTML)
 
-// if (sumBonus >= 2000) {
-//       alert(`I'm ready to work`) 
-// } else {
-//    alert(`I'm out`) 
-// } 
+let a = document.createElement('a')
+a.setAttribute('id', 'link');
+a.setAttribute('href', 'https://dom.spec.whatwg.org/') 
 
-console.log(sumBonus >= 2000);
+ a.innerHTML = h1.outerHTML
+ console.log(a.outerHTML)//<a id="link" href="https://dom.spec.whatwg.org/"><h1>Document Object Model<span>DOM</span> </h1></a>
